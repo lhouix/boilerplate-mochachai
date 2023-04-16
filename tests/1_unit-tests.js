dd047d3db7c38af709b804c0705d109b50ec444e
lhouix <80489961+lhouix@users.noreply.github.com>
2023-04-16 18:12:13 +0200

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -10,9 +10,9 @@ suite('Unit Tests', function () {
     });
     // #2
     test('#isDefined, #isUndefined', function () {
-      assert.isDefined(null, 'null is not undefined');
-      assert.isUndefined(undefined, 'undefined IS undefined');
-      assert.isDefined('hello', 'A string is not undefined');
+      assert.isdefined(null, 'null is not undefined');
+      assert.isundefined(undefined, 'undefined IS undefined');
+      assert.isdefined('hello', 'A string is not undefined');
     });
     // #3
     test('#isOk, #isNotOk', function () {
